1294b537e44fb95727f87acd68462fd827db99f9
Philip Stafford Wood <philip.wood@gmail.com>
2020-06-08 11:32:18 +0200

    updating command wording and fixing OPA DeployBundle Nil panic

--- cmd/namespace.go
+++ cmd/namespace.go
@@ -19,20 +19,20 @@ var timeout time.Duration
 
 var namespaceForceDelete = &cobra.Command{
 	Use:   "force-delete",
-	Short: "Clears the namespace finalizers allowing it to be deleted",
+	Short: "Forcibly deletes a namespace",
 	Args:  cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		p := getPlatform(cmd)
 
 		ns := args[0]
-		fmt.Printf("Clearing finalizers on namespace %v\n", ns)
+		fmt.Printf("Clearing finalizers and deleteing namespace %v\n", ns)
 
 		duration := durafmt.Parse(timeout).String()
 		fmt.Printf("Using timeout of %v\n", duration)
 
 		err := p.ForceDeleteNamespace(ns, timeout)
 		if err != nil {
-			return fmt.Errorf("clearing the finalizers failed with: %v", err)
+			return fmt.Errorf("force deletion failed with: %v", err)
 		}
 
 		return nil
